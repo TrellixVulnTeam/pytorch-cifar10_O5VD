0f77fd087f4b716a860c1e53d9ae9e2ed34cb0c1
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-06-01 16:22:09 +0300

    GPU Out of Memory (smaller model chosen)

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,5 @@
 from .alexnet import alexnet as alexnet
-from .bit import bitmr152x4 as bit
+from .bit import bitmr101x1 as bit
 from .densenet import densenet201 as densenet
 from .googlenet import googlenet as googlenet
 from .resnet import resnet152 as resnet
