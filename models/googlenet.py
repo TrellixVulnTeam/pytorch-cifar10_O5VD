40bede3c4710a30ca916c4ee53b7a689b7e7176d
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-06-01 14:42:22 +0300

    Optimized for cifar10

--- models/googlenet.py
+++ models/googlenet.py
@@ -63,8 +63,7 @@ def googlenet(pretrained=False, progress=True, **kwargs):
 class GoogLeNet(nn.Module):
     __constants__ = ['aux_logits', 'transform_input']
 
-    def __init__(self, num_classes=1000, aux_logits=True, transform_input=False, init_weights=None,
-                 blocks=None):
+    def __init__(self, num_classes=10, aux_logits=False, transform_input=False, init_weights=False, blocks=None):
         super(GoogLeNet, self).__init__()
         if blocks is None:
             blocks = [BasicConv2d, Inception, InceptionAux]
@@ -81,22 +80,22 @@ class GoogLeNet(nn.Module):
         self.aux_logits = aux_logits
         self.transform_input = transform_input
 
-        self.conv1 = conv_block(3, 64, kernel_size=7, stride=2, padding=3)
-        self.maxpool1 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
-        self.conv2 = conv_block(64, 64, kernel_size=1)
-        self.conv3 = conv_block(64, 192, kernel_size=3, padding=1)
-        self.maxpool2 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+        self.conv1 = conv_block(3, 192, kernel_size=3, stride=1, padding=1)
+        # self.maxpool1 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+        # self.conv2 = conv_block(64, 64, kernel_size=1)
+        # self.conv3 = conv_block(64, 192, kernel_size=3, padding=1)
+        # self.maxpool2 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
 
         self.inception3a = inception_block(192, 64, 96, 128, 16, 32, 32)
         self.inception3b = inception_block(256, 128, 128, 192, 32, 96, 64)
-        self.maxpool3 = nn.MaxPool2d(3, stride=2, ceil_mode=True)
+        self.maxpool3 = nn.MaxPool2d(3, stride=2, padding=1, ceil_mode=False)
 
         self.inception4a = inception_block(480, 192, 96, 208, 16, 48, 64)
         self.inception4b = inception_block(512, 160, 112, 224, 24, 64, 64)
         self.inception4c = inception_block(512, 128, 128, 256, 24, 64, 64)
         self.inception4d = inception_block(512, 112, 144, 288, 32, 64, 64)
         self.inception4e = inception_block(528, 256, 160, 320, 32, 128, 128)
-        self.maxpool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)
+        self.maxpool4 = nn.MaxPool2d(3, stride=2, padding=1, ceil_mode=False)
 
         self.inception5a = inception_block(832, 256, 160, 320, 32, 128, 128)
         self.inception5b = inception_block(832, 384, 192, 384, 48, 128, 128)
@@ -142,13 +141,13 @@ class GoogLeNet(nn.Module):
         # N x 3 x 224 x 224
         x = self.conv1(x)
         # N x 64 x 112 x 112
-        x = self.maxpool1(x)
+        # x = self.maxpool1(x)
         # N x 64 x 56 x 56
-        x = self.conv2(x)
+        # x = self.conv2(x)
         # N x 64 x 56 x 56
-        x = self.conv3(x)
+        # x = self.conv3(x)
         # N x 192 x 56 x 56
-        x = self.maxpool2(x)
+        # x = self.maxpool2(x)
 
         # N x 192 x 28 x 28
         x = self.inception3a(x)
