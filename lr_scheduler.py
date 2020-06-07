4f9b89dc68a1a6b5fbfcfb1c837b5fa56518d823
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-06-07 22:57:35 +0300

    LR csv v2

--- lr_scheduler.py
+++ lr_scheduler.py
@@ -1,5 +1,18 @@
 from utils import Logger
 
+
+def learning_scheduler(lr):
+    if epoch < epochs[0]:
+        lr = lr * (step + 1 + total_step * epoch) / (total_step * epochs[0])
+    elif epoch >= epochs[-1]:
+        lr = None
+    else:
+        for s in epochs[1:]:
+            if s < epoch:
+                lr /= 10
+    return lr
+
+
 lr = 0.003
 epochs = [1, 5, 10, 15, 20]
 total_step = 391
@@ -8,14 +21,6 @@ logger.header("epoch, step, lr")
 
 for epoch in range(0, epochs[-1]):
     for step in range(0, total_step):
-        if epoch < epochs[0]:
-            lr = lr * (step + 1 + total_step * epoch) / (total_step * epochs[0])
-        elif epoch >= epochs[-1]:
-            lr = None
-        else:
-            for s in epochs[1:]:
-                if s < epoch:
-                    lr /= 10
         logger.write('{}, {}, {:.10f}\n'.format(epoch + 1,
                                                 step + 1,
-                                                lr))
+                                                learning_scheduler(lr)))
