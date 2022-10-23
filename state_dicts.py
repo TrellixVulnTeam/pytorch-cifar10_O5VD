030f833e356bdfd4ec042bb40d1fd8d81296c16a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-23 08:41:20 +0000

    Adding tarfile member sanitization to extractall()

--- state_dicts.py
+++ state_dicts.py
@@ -52,7 +52,26 @@ class StateDicts:
     def extract(self):
         path_to_tar_file = os.path.join(os.getcwd(), 'state_dicts.tar.gz')
         with tarfile.open(path_to_tar_file, 'r') as archive:
-            archive.extractall(os.getcwd())
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, os.getcwd())
         print('Extract successful!')
 
     def remove(self):
